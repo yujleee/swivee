b73f1a434334428023c686c870200f62da8afb2e
tjddkwh96@naver.com <tjddkwh96@naver.com>
2022-11-22 14:49:42 +0900

    [Design] js, mypage 변경
    main에 import 추가,  onchange 위치 변경

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ import {
   goToMypage,
 } from './router.js';
 import { authService } from './firebase.js';
+import { onFileChange } from './mypage.js';
 
 // url 바뀌면 handleLocation 실행하여 화면 변경
 window.addEventListener('hashchange', handleLocation);
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function () {
       // 로그인 상태이므로 항상 팬명록 화면으로 이동
       if (hash === '') {
         // 로그인 상태에서는 로그인 화면으로 되돌아갈 수 없게 설정
-        window.location.replace('#fanLog');
+        window.location.replace('#mypage');
       }
     } else {
       // 로그아웃 상태이므로 로그인 화면으로 강제 이동
@@ -41,3 +42,4 @@ window.goToJoin = goToJoin;
 window.goToBoard = goToBoard;
 window.goToReview = goToReview;
 window.goToMypage = goToMypage;
+window.onFileChange = onFileChange;
