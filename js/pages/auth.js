3aae003ca122f07ed251dc156316e30def2bd161
dada992 <olive_js_933@naver.com>
2022-11-22 19:01:57 +0900

    [feat] 여러 가지 추가

--- js/pages/auth.js
+++ js/pages/auth.js
@@ -96,6 +96,7 @@ export const socialLogin = (str) => {
   signInWithPopup(authService, provider)
     .then((result) => {
       const user = result.user;
+      window.location.hash = "#";
     })
     .catch((error) => {
       // Handle Errors here.
