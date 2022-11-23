2de51ba45839651e64e1b1e71f29d792a76b9054
LeeYujeong <yujlee242@gmail.com>
2022-11-23 19:10:08 +0900

    [Feat] 실시간 리뷰 리스트 기능 추가

--- js/main.js
+++ js/main.js
@@ -1,78 +1,72 @@
-import {
-    handleLocation,
-    goToLogin,
-    goToJoin,
-    goToBoard,
-    goToReview,
-    goToMypage,
-  } from './router.js';
-  import { handleAuth, socialLogin, logout } from './pages/auth.js';
-  import { authService } from './firebase.js';
-  import { changeProfile, imgFileUpload, saveReview, receiveDataFromMain } from './board.js';
-  import { toggleMoreBrand, changeShoesList, showMoreShoes, getBrandList } from './pages/home.js';
-  import { onFileChange, changeProfiles, onChangeNickname } from './mypage.js';
-  
-  const activeMenu = document.querySelector('.active');
-  
-  // url 바뀌면 handleLocation 실행하여 화면 변경
-  window.addEventListener('hashchange', handleLocation);
-  
-  // 첫 랜딩 또는 새로고침 시 handleLocation 실행하여 화면 변경
-  document.addEventListener('DOMContentLoaded', function () {
-    getBrandList();
-    changeShoesList();
-  
-    // 인증 관련 수정 추가 필요!
-    // Firebase 연결상태를 감시
-    authService.onAuthStateChanged((user) => {
-      // Firebase 연결되면 화면 표시
-      handleLocation();
-      const hash = window.location.hash;
-      if (user) {
-        // 로그인 상태이므로 항상 팬명록 화면으로 이동
-        console.log(user);
-  
-        activeMenu.textContent = 'Logout';
-        activeMenu.setAttribute('onclick', 'logout()');
-  
-        if (hash === '') {
-          // 로그인 상태에서는 로그인 화면으로 되돌아갈 수 없게 설정
-          window.location.replace('#');
-        }
-      } else {
-        // 로그아웃 상태이므로 로그인 화면으로 강제 이동
-        if (hash !== '') {
-          window.location.replace('');
-        }
-  
-        activeMenu.textContent = 'Login';
-        activeMenu.setAttribute('onclick', 'goToLogin()');
+import { handleLocation, goToLogin, goToJoin, goToBoard, goToReview, goToMypage } from './router.js';
+import { handleAuth, socialLogin, logout } from './pages/auth.js';
+import { authService } from './firebase.js';
+import { changeProfile, imgFileUpload, saveReview, receiveDataFromMain } from './board.js';
+import { toggleMoreBrand, changeShoesList, showMoreShoes, getBrandList, getRealtimeReviews } from './pages/home.js';
+import { onFileChange, changeProfiles, onChangeNickname } from './mypage.js';
+
+const activeMenu = document.querySelector('.active');
+
+// url 바뀌면 handleLocation 실행하여 화면 변경
+window.addEventListener('hashchange', handleLocation);
+
+// 첫 랜딩 또는 새로고침 시 handleLocation 실행하여 화면 변경
+document.addEventListener('DOMContentLoaded', function () {
+  getBrandList();
+  changeShoesList();
+  getRealtimeReviews();
+
+  // 인증 관련 수정 추가 필요!
+  // Firebase 연결상태를 감시
+  authService.onAuthStateChanged((user) => {
+    // Firebase 연결되면 화면 표시
+    handleLocation();
+    const hash = window.location.hash;
+    if (user) {
+      // 로그인 상태이므로 항상 팬명록 화면으로 이동
+      console.log(user);
+
+      activeMenu.textContent = 'Logout';
+      activeMenu.setAttribute('onclick', 'logout()');
+
+      if (hash === '') {
+        // 로그인 상태에서는 로그인 화면으로 되돌아갈 수 없게 설정
+        window.location.replace('#');
       }
-    });
+    } else {
+      // 로그아웃 상태이므로 로그인 화면으로 강제 이동
+      if (hash !== '') {
+        window.location.replace('');
+      }
+
+      activeMenu.textContent = 'Login';
+      activeMenu.setAttribute('onclick', 'goToLogin()');
+    }
   });
-  
-  // onclick, onchange, onsubmit 이벤트 핸들러 리스트
-  // 페이지 이동 핸들러 리스트들
-  window.goToLogin = goToLogin;
-  window.goToJoin = goToJoin;
-  window.goToBoard = goToBoard;
-  window.goToReview = goToReview;
-  window.goToMypage = goToMypage;
-  window.onFileChange = onFileChange;
-  window.changeProfile = changeProfile;
-  window.toggleMoreBrand = toggleMoreBrand;
-  
-  window.handleAuth = handleAuth;
-  window.socialLogin = socialLogin;
-  window.changeShoesList = changeShoesList;
-  window.showMoreShoes = showMoreShoes;
-  window.imgFileUpload = imgFileUpload;
-  window.saveReview = saveReview;
-  
-  window.receiveDataFromMain = receiveDataFromMain;
-  // window.getReviewList = getReviewList;
-  window.changeProfiles = changeProfiles;
-  window.logout = logout;
-  window.getBrandList = getBrandList;
-  window.onChangeNickname = onChangeNickname;
-  
+});
+
+// onclick, onchange, onsubmit 이벤트 핸들러 리스트
+// 페이지 이동 핸들러 리스트들
+window.goToLogin = goToLogin;
+window.goToJoin = goToJoin;
+window.goToBoard = goToBoard;
+window.goToReview = goToReview;
+window.goToMypage = goToMypage;
+window.onFileChange = onFileChange;
+window.changeProfile = changeProfile;
+window.toggleMoreBrand = toggleMoreBrand;
+
+window.handleAuth = handleAuth;
+window.socialLogin = socialLogin;
+window.changeShoesList = changeShoesList;
+window.showMoreShoes = showMoreShoes;
+window.imgFileUpload = imgFileUpload;
+window.saveReview = saveReview;
+
+window.receiveDataFromMain = receiveDataFromMain;
+// window.getReviewList = getReviewList;
+window.changeProfiles = changeProfiles;
+window.logout = logout;
+window.getBrandList = getBrandList;
+window.onChangeNickname = onChangeNickname;
+window.getRealtimeReviews = getRealtimeReviews;
