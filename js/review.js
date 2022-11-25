b984ef22a9db524be138d7d56a5619061c0bcd3d
tjddkwh96@naver.com <tjddkwh96@naver.com>
2022-11-25 12:28:18 +0900

    [feat] 버튼 수정 기능 추가

--- js/review.js
+++ js/review.js
@@ -1,4 +1,4 @@
-import { dbService, authService, storageService } from "./firebase.js";
+import { dbService, authService, storageService } from './firebase.js';
 import {
   doc,
   addDoc,
@@ -8,59 +8,62 @@ import {
   orderBy,
   query,
   getDocs,
-} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+} from 'https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js';
 
-export const saveComment = async (event) => {
+export const saveComment = async event => {
   event.preventDefault();
-  const comment = document.getElementById("commentInput");
+  const comment = document.getElementById('commentInput');
   const { uid, photoURL, displayName } = authService.currentUser;
   try {
-    await addDoc(collection(dbService, "comments"), {
+    await addDoc(collection(dbService, 'comments'), {
       text: comment.value,
       createdAt: Date.now(),
       creatorId: uid,
       profileImg: photoURL,
       nickname: displayName,
     });
-    comment.value = "";
+    comment.value = '';
     getCommentList();
   } catch (error) {
     alert(error);
-    console.log("error in addDoc:", error);
+    console.log('error in addDoc:', error);
   }
 };
 
 //수정, 삭제 부분
-
-export const onEditing = (event) => {
+export const onEditing = event => {
   // 수정버튼 클릭
   event.preventDefault();
-  const udBtns = document.querySelectorAll(".editBtn, .deleteBtn");
-  udBtns.forEach((udBtn) => (udBtn.disabled = "true"));
-
-  const cardBody = event.target.parentNode.parentNode;
-  const commentText = cardBody.children[0].children[0];
-  const commentInputP = cardBody.children[0].children[1];
-
-  commentText.classList.add("noDisplay");
-  commentInputP.classList.add("d-flex");
-  commentInputP.classList.remove("noDisplay");
-  commentInputP.children[0].focus();
+  const udBtns = document.querySelectorAll('.editBtn, .deleteBtn');
+  udBtns.forEach(udBtn => (udBtn.disabled = 'true'));
+  console.log(udBtns)
+  const cardBody = event.target.parentNode.parentNode; //cardbody = 수정 버튼
+  console.log(cardBody);
+  const commentText = cardBody.children[0];
+  const commentInputP = cardBody.children[1];
+  commentText.classList.add('noDisplay');
+  // commentInputP.classList.add('d-flex');
+  commentInputP.classList.remove('noDisplay');
+  console.log(commentInputP);
+  commentInputP.focus();
+  udBtns.forEach(udBtns=>(udBtns.classList.add("noDisplay")))
 };
 
-export const update_comment = async (event) => {
+
+
+export const update_comment = async event => {
   event.preventDefault();
   const newComment = event.target.parentNode.children[0].value;
   const id = event.target.parentNode.id;
 
   const parentNode = event.target.parentNode.parentNode;
   const commentText = parentNode.children[0];
-  commentText.classList.remove("noDisplay");
+  commentText.classList.remove('noDisplay'); //수정input display:none
   const commentInputP = parentNode.children[1];
-  commentInputP.classList.remove("d-flex");
-  commentInputP.classList.add("noDisplay");
+  commentInputP.classList.remove('d-flex');
+  commentInputP.classList.add('noDisplay');
 
-  const commentRef = doc(dbService, "comments", id);
+  const commentRef = doc(dbService, 'comments', id);
   try {
     await updateDoc(commentRef, { text: newComment });
     getCommentList();
@@ -69,13 +72,13 @@ export const update_comment = async (event) => {
   }
 };
 
-export const delete_comment = async (event) => {
+export const delete_comment = async event => {
   event.preventDefault();
   const id = event.target.name;
-  const ok = window.confirm("해당 응원글을 정말 삭제하시겠습니까?");
+  const ok = window.confirm('해당 응원글을 정말 삭제하시겠습니까?');
   if (ok) {
     try {
-      await deleteDoc(doc(dbService, "comments", id));
+      await deleteDoc(doc(dbService, 'comments', id));
       getCommentList();
     } catch (error) {
       alert(error);
@@ -115,26 +118,27 @@ export const delete_comment = async (event) => {
 export const getCommentList = async () => {
   let cmtObjList = [];
   const q = query(
-    collection(dbService, "comments"),
-    orderBy("createdAt", "desc")
+    collection(dbService, 'comments'),
+    orderBy('createdAt', 'desc')
   );
   const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
+  querySnapshot.forEach(doc => {
     const commentObj = {
       id: doc.id,
       ...doc.data(),
     };
     cmtObjList.push(commentObj);
   });
-  const commentList = document.getElementById("commentList1");
+  const commentList = document.getElementById('commentList1');
   const currentUid = authService.currentUser.uid;
-  commentList.innerHTML = "";
-  cmtObjList.forEach((cmtObj) => {
+  commentList.innerHTML = '';
+  cmtObjList.forEach(cmtObj => {
     const isOwner = currentUid === cmtObj.creatorId;
-    const temp_html = `<div class="reviewListComment">
+    const temp_html = `
+    <div class="reviewListComment">
     <div class="reviewListBox">
       <img class="cardEmoticon" src="${
-        cmtObj.profileImg ?? "/assets/blank-profile-picture.png"
+        cmtObj.profileImg ?? '/assets/blank-profile-picture.png'
       }" alt="" />
       <div class="reviewListBoxNameDate">
         <div class="reviewListBoxName">${cmtObj.nickname}</div>
@@ -148,20 +152,19 @@ export const getCommentList = async () => {
     <p id="${
       cmtObj.id
     }" class="noDisplay"><input class="newCmtInput" type="text" maxlength="30" /><button class="updateBtn" onclick="update_comment(event)">완료</button></p>
-    <div class="${isOwner ? "updateBtns" : "noDisplay"}">
+    <div id="editbtns" class="${isOwner ? 'updateBtns' : 'noDisplay'}">
     <button onclick="onEditing(event)" class="editBtn">수정</button>
     <button
       name="${cmtObj.id}"
       onclick="delete_comment(event)"
-      class="deleteBtn"
-    >
+      class="deleteBtn">
       삭제
     </button>
   </div>
   </div>`;
-    console.log("commentList", commentList);
-    const div = document.createElement("div");
-    div.classList.add("mycards");
+    console.log('commentList', commentList);
+    const div = document.createElement('div');
+    div.classList.add('mycards');
     div.innerHTML = temp_html;
     commentList.appendChild(div);
   });
