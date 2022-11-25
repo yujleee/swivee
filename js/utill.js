8c49361a1016343a2b62def7292d2ed339f34181
LeeYujeong <yujlee242@gmail.com>
2022-11-25 12:28:05 +0900

    [feat] 신발별 유튜브 url 링크 적용되도록 추가

--- js/utill.js
+++ js/utill.js
@@ -21,5 +21,16 @@
 //유효성 검사
 
 export const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-export const pwRegex =
-  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+export const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+
+// 신발 유튜브 영상 링크
+export const searchOnYoutube = (shoesName) => {
+  const pattern = /\s/g; // 공백 체크하는 정규표현식
+
+  if (shoesName.match(pattern)) {
+    const keyword = shoesName.replace(/^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/).replace(/\s/g, '+');
+    return `https://www.youtube.com/results?search_query=${keyword}`;
+  } else {
+    return `https://www.youtube.com/results?search_query=${shoesName}`;
+  }
+};
